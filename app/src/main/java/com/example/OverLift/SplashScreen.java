b9f00323e1b2a2b8fc91ee3766500bb4443f12a9
DarrenCoughlan <darren.coughlan@ucdconnect.ie>
2019-10-19 20:43:18 +0100

    First commit

---
+++ app/src/main/java/com/example/OverLift/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.OverLift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+    /**
+     * Show an Enter Growl message
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view) {
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Enter!", Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+}
